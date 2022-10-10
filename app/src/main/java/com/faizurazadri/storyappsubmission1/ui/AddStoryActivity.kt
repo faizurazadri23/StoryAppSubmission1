c7cfa40d50543fc17e07b514b0233568ea60c7ae
Faizura Zadri <faizura@explorindo.com>
2022-10-10 09:04:21 +0700

    logout and ui add story

--- app/src/main/java/com/faizurazadri/storyappsubmission1/ui/AddStoryActivity.kt
+++ app/src/main/java/com/faizurazadri/storyappsubmission1/ui/AddStoryActivity.kt
@@ -3,10 +3,15 @@ package com.faizurazadri.storyappsubmission1.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.faizurazadri.storyappsubmission1.R
+import com.faizurazadri.storyappsubmission1.databinding.ActivityAddStoryBinding
 
 class AddStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddStoryBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/faizurazadri/storyappsubmission1/ui/ListStoryActivity.kt
+++ app/src/main/java/com/faizurazadri/storyappsubmission1/ui/ListStoryActivity.kt
@@ -1,13 +1,14 @@
 package com.faizurazadri.storyappsubmission1.ui
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.faizurazadri.storyappsubmission1.R
 import com.faizurazadri.storyappsubmission1.adapter.AdapterStory
@@ -34,7 +35,8 @@ class ListStoryActivity : AppCompatActivity() {
         val sharedPreference = getSharedPreferences("auth", Context.MODE_PRIVATE)
 
 
-        val userData = Gson().fromJson(sharedPreference.getString("user", ""), LoginResult::class.java)
+        val userData =
+            Gson().fromJson(sharedPreference.getString("user", ""), LoginResult::class.java)
 
         userData.token?.let { storyViewModel.getAllStories(it) }
 
@@ -65,10 +67,47 @@ class ListStoryActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.exit_application -> {
+            R.id.action_logout -> {
+                showAlertDialogLogout()
+                true
+            }
+
+            R.id.add_story -> {
+                Intent(this, AddStoryActivity::class.java).also { startActivity(it) }
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showAlertDialogLogout() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(applicationContext.getString(R.string.keluar))
+        builder.setMessage(applicationContext.getString(R.string.description_alert_logout))
+        builder.setPositiveButton(android.R.string.yes) { _, _ ->
+
+            logout()
+        }
+
+        builder.setNegativeButton(android.R.string.no) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.show()
+    }
+
+    private fun logout() {
+        val sharedPreference = getSharedPreferences("auth", Context.MODE_PRIVATE)
+        val editor = sharedPreference.edit()
+        editor.remove("user")
+        editor.clear()
+        editor.apply()
+
+
+        Intent(this, LoginActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(it)
+            finish()
         }
     }
 }
